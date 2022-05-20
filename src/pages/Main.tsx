edafff9879adc28cad25d76fbeb0de48521de966
SEBFull-Jeongmin <lee.jeongmin1625@gmail.com>
2022-05-21 00:29:19 +0900

    메인페이지 css부분

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { Type } from "../styles/Main";
+import {
+  MenuLink,
+  Title,
+  Type,
+  Img,
+  CardImg,
+  Container,
+  Button,
+} from "../styles/Main";
 const Main = () => {
   return (
-    <div>
-      <div>
-        <img src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[110563]_20210426095937808.jpg" />
-        음료
-      </div>
-
-      <div>
-        <img src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[5110007181]_20210421164728664.jpg" />
-        디저트
-      </div>
-      <div>
-        <img src="https://image.istarbucks.co.kr/upload/store/skuimg/2020/07/[11108699]_20200724095028789.jpg" />
-        굿즈
-      </div>
-      <div>
-        <img src="https://image.istarbucks.co.kr/cardImg/20220315/008947_WEB.png" />
-        기프트카드
-      </div>
-    </div>
+    <Container>
+      <Button>
+        <MenuLink to="javascript:void(0)">
+          <Type>
+            <Img src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[110563]_20210426095937808.jpg" />
+            <Title>음료</Title>
+          </Type>
+        </MenuLink>
+        <MenuLink to="javascript:void(0)">
+          <Type>
+            <Img src="https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[5110007181]_20210421164728664.jpg" />
+            <Title>디저트</Title>
+          </Type>
+        </MenuLink>
+      </Button>
+      <Button>
+        <MenuLink to="javascript:void(0)">
+          <Type>
+            <Img src="https://image.istarbucks.co.kr/upload/store/skuimg/2020/07/[11108699]_20200724095028789.jpg" />
+            <Title>굿즈</Title>
+          </Type>
+        </MenuLink>{" "}
+        <MenuLink to="javascript:void(0)">
+          <Type>
+            <CardImg src="https://image.istarbucks.co.kr/cardImg/20220315/008947_WEB.png" />
+            <Title>기프트카드</Title>
+          </Type>
+        </MenuLink>
+      </Button>
+    </Container>
   );
 };
 
